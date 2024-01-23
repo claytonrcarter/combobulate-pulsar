8d81dd972eacc675d1da4a155e7373874e407ffb
Clayton Carter <claytonrcarter@gmail.com>
2024-01-22 22:55:37 -0500

    feat: transposing nodes

--- spec/combobulate-spec.js
+++ spec/combobulate-spec.js
@@ -14,6 +14,19 @@ function getNodeAtCursor(editor) {
     .getSyntaxNodeAtPosition(editor.getCursorBufferPosition());
 }
 
+function describeNode(node) {
+  if (!node) {
+    return 'Bad node!';
+  }
+
+  return {
+    text: node.text,
+    type: node.type,
+    range: node.range,
+    isNamed: node.isNamed(),
+  };
+}
+
 describe('CombobulatePulsar', () => {
   let workspaceElement, editor;
 
@@ -104,6 +117,134 @@ describe('CombobulatePulsar', () => {
 
         expect(editor.getCursorBufferPosition().column).toBe(17);
         expect(getNodeAtCursor(editor).text).toBe('3');
+      });
+    });
+  });
+
+  describe('swapping nodes', () => {
+    describe('sibling nodes', () => {
+      it('swaps simple nodes', async () => {
+        await setText(editor, '[1, 2, 3]');
+        editor.setCursorBufferPosition([0, 4]);
+
+        expect(getNodeAtCursor(editor).text).toBe('2');
+        expect(getNodeAtCursor(editor).nextNamedSibling.text).toBe('3');
+
+        Combobulate.swapWithNextSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(7);
+        expect(editor.getText()).toBe('[1, 3, 2]');
+
+        Combobulate.swapWithPreviousSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(4);
+        expect(editor.getText()).toBe('[1, 2, 3]');
+      });
+
+      it('swaps nodes of different length', async () => {
+        await setText(editor, '[a, bc, def]');
+        editor.setCursorBufferPosition([0, 4]);
+
+        Combobulate.swapWithNextSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(9);
+        expect(editor.getText()).toBe('[a, def, bc]');
+
+        Combobulate.swapWithPreviousSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(4);
+        expect(editor.getText()).toBe('[a, bc, def]');
+      });
+
+      it('swaps complex nodes', async () => {
+        await setText(editor, '["abc", {foo: 1}, Math.abs(-1)]');
+        editor.setCursorBufferPosition([0, 8]);
+
+        Combobulate.swapWithNextSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(22);
+        expect(editor.getText()).toBe('["abc", Math.abs(-1), {foo: 1}]');
+
+        Combobulate.swapWithPreviousSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(8);
+        expect(editor.getText()).toBe('["abc", {foo: 1}, Math.abs(-1)]');
+      });
+
+      it('doesnt swap with next when at last node', async () => {
+        await setText(editor, '[1, 2, 3]');
+        editor.setCursorBufferPosition([0, 7]);
+
+        expect(getNodeAtCursor(editor).text).toBe('3');
+
+        Combobulate.swapWithNextSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(7);
+        expect(getNodeAtCursor(editor).text).toBe('3');
+      });
+
+      it('doesnt swap with previous when at first node', async () => {
+        await setText(editor, '[1, 2, 3]');
+        editor.setCursorBufferPosition([0, 1]);
+
+        expect(getNodeAtCursor(editor).text).toBe('1');
+
+        Combobulate.swapWithPreviousSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(1);
+        expect(getNodeAtCursor(editor).text).toBe('1');
+      });
+
+      it('undoes transpositions in a single action', async () => {
+        await setText(editor, '[1, 2, 3]');
+        editor.setCursorBufferPosition([0, 1]);
+
+        expect(getNodeAtCursor(editor).text).toBe('1');
+        expect(getNodeAtCursor(editor).nextNamedSibling.text).toBe('2');
+
+        Combobulate.swapWithNextSibling();
+
+        expect(editor.getCursorBufferPosition().column).toBe(4);
+        expect(editor.getText()).toBe('[2, 1, 3]');
+
+        editor.undo();
+
+        expect(editor.getCursorBufferPosition().column).toBe(1);
+        expect(editor.getText()).toBe('[1, 2, 3]');
+      });
+    });
+
+    describe('transposing nodes', () => {
+      it('transposes previous node when at start of node', async () => {
+        await setText(editor, '[a, bcd, ef]');
+        editor.setCursorBufferPosition([0, 4]);
+
+        Combobulate.transposeNodes();
+
+        expect(editor.getCursorBufferPosition().column).toBe(6);
+        expect(editor.getText()).toBe('[bcd, a, ef]');
+      });
+
+      it('transposes previous node when in middle of node', async () => {
+        await setText(editor, '[a, bcd, ef]');
+        editor.setCursorBufferPosition([0, 6]);
+
+        Combobulate.transposeNodes();
+
+        // FIXME is this correct? the cursor is moving to the end of the second
+        // node
+        expect(editor.getCursorBufferPosition().column).toBe(7);
+        expect(editor.getText()).toBe('[bcd, a, ef]');
+      });
+
+      it('transposes next node when at end of node', async () => {
+        await setText(editor, '[a, bcd, ef]');
+        editor.setCursorBufferPosition([0, 7]);
+
+        Combobulate.transposeNodes();
+
+        expect(editor.getCursorBufferPosition().column).toBe(6);
+        expect(editor.getText()).toBe('[a, ef, bcd]');
       });
     });
   });
